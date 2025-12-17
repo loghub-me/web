3ce9d27f73efc034e4fdd88b4584d4a1ffc03318
gymynnym <gymynnym@gmail.com>
2025-12-18 00:07:08 +0900

    refactor(ui): Normalize input and textarea text size to sm
    - Bump version to 0.6.4

--- components/ui/share-menu.tsx
+++ components/ui/share-menu.tsx
@@ -16,10 +16,8 @@ interface ShareMenuProps {
 function ShareMenu({ shareURLs }: Readonly<ShareMenuProps>) {
   return (
     <DropdownMenu modal={false}>
-      <DropdownMenuTrigger
-        render={<Button variant={'ghost'} size={'icon'} className="size-10 data-[state=open]:[&_svg]:rotate-90" />}
-      >
-        <Share2Icon className="transition-transform" />
+      <DropdownMenuTrigger render={<Button variant={'ghost'} size={'icon'} className="size-10" />}>
+        <Share2Icon />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="p-0 min-w-auto ring-0 bg-transparent">
         <ButtonGroup orientation={'vertical'}>
